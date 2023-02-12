7c514446c28b55e9473ffaff23aaee56bc888a7c
Błażej Kowol <kowol.blazej@gmail.com>
2023-02-12 01:41:01 +0100

    add search option with an issue

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,11 +1,21 @@
 import styles from './SearchForm.module.scss';
 import TextInput from '../TextInput/TextInput';
 import Button from '../Button/Button';
+import { useDispatch } from 'react-redux';
+import { useState } from 'react';
 
 const SearchForm = () => {
+
+    const [search, setSearch] = useState('');
+    const dispatch = useDispatch();
+    const handleSubmit = e => {
+        e.preventDefault();
+        dispatch({ type: 'SEARCH_INPUT',  payload: { search } });
+    }
+
     return (
-        <form className={styles.searchForm}>
-            <TextInput placeholder="Search..."/>
+        <form className={styles.searchForm} onSubmit={handleSubmit}>
+            <TextInput onChange={e => setSearch(e.target.value)} placeholder="Search..."/>
             <Button>
                 <span className='fa fa-search'/>
             </Button>
@@ -14,3 +24,9 @@ const SearchForm = () => {
   };
 
   export default SearchForm;
+
+  /*const [text, setText] = useState();
+  const handleSubmit = e => {
+      e.preventDefault();
+      dispatch({ type: 'SEARCH_INPUT', payload: { text } });
+      setText('');*/

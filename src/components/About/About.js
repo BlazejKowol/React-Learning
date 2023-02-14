3805385163d59acc0c6b32548f28443d159f531e
Błażej Kowol <kowol.blazej@gmail.com>
2023-02-14 14:30:51 +0100

    create About & Favorite

---
+++ src/components/About/About.js
@@ -0,0 +1,12 @@
+import styles from './About.module.scss';
+
+const About= () => {
+  return (
+    <div className={styles.hero}>
+      <h1 className={styles.title}>About</h1>
+      <p className={styles.subtitle}>Lorem Ipsum</p>
+    </div>
+  );
+};
+
+  export default About;
